8e1d4a05bbeb0f4ef86ae6cf1456299c6b3c4d25
otaly <60144056+otaly@users.noreply.github.com>
2025-02-25 17:17:50 +0900

    eslint, prettier追加

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/*',
+      '.next/*',
+      'public/*',
+      'pnpm-lock.yaml',
+      'postcss.config.js',
+    ],
+  },
+];
